8e26c1ad9a88803c9d53e6ab540c79a9ac1d0f14
DharshanSR <dharshanravindran8@gmail.com>
2025-05-01 13:01:43 +0530

    feat(core): implement Mentor class extending User

--- src/Mentor.java
+++ src/Mentor.java
@@ -1,2 +1,24 @@
-public class Mentor {
+public class Mentor extends User{
+
+    private String expertise;
+
+    public Mentor(String name, String expertise) {
+        super(name);
+        this.expertise = expertise;
+    }
+
+    public String getExpertise(){
+        return expertise;
+    }
+
+    @Override
+    public String getRole(){
+        return "Mentor";
+    }
+
+    @Override
+    public String toString() {
+        return "Mentor[ID=" + getId() + ", Name=" + getName() +
+                ", Expertise=" + expertise + "]";
+    }
 }
