21a0a30cdc237ca5f618499d8b47936d15543278
DharshanSR <dharshanravindran8@gmail.com>
2025-05-01 12:53:24 +0530

    feat(core): add abstract User base class with ID, name, and role structure

--- src/User.java
+++ src/User.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+public abstract class User {
+    private int id;
+    private String name;
+    private static int nextId = 1;
 
-public class User {
+    public User (String name) {
+        this.id = nextId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String getRole();
+
+    @Override
+    public String toString() {
+        return getRole() + "[ID = " + id + ", Name = " + name + "]";
+    }
 }
