48dea5dda641c7a68f5b7b9ead65540470e67430
DharshanSR <dharshanravindran8@gmail.com>
2025-05-03 21:01:18 +0530

    Adding the comments for the user and subjectsession easy to understand

--- src/SubjectSession.java
+++ src/SubjectSession.java
@@ -1,21 +1,30 @@
-public class SubjectSession extends Session{
+public class SubjectSession extends Session {
 
+    // The name of the subject for this session
     private String subjectName;
 
-    public SubjectSession (Student student, Mentor mentor, String subjectName, String dateTime) {
+    // Constructor to create a new SubjectSession
+    public SubjectSession(Student student, Mentor mentor, String subjectName, String dateTime) {
+        // Call the constructor of the parent class (Session)
         super(student, mentor, dateTime);
+        // Set the subject name for this session
         this.subjectName = subjectName;
     }
 
+    // Getter method to get the subject name
     public String getSubjectName() {
         return subjectName;
     }
 
+    // This method overrides getType() from the Session class
+    // It tells what type of session this is
     @Override
     public String getType() {
         return "Subject 1-to-1";
     }
 
+    // This method overrides getDetails() from the Session class
+    // It returns more info about the session (the subject)
     @Override
     public String getDetails() {
         return "Subject: " + subjectName;
--- src/User.java
+++ src/User.java
@@ -1,23 +1,35 @@
 public abstract class User {
+    // Each user has an ID and a name
     private int id;
     private String name;
+
+    // Static variable to keep track of the next ID to assign
     private static int nextId = 1;
 
-    public User (String name) {
+    // Constructor to create a new User
+    public User(String name) {
+        // Assign a unique ID using nextId
         this.id = nextId;
+        // Store the name
         this.name = name;
+        // Increase nextId so the next user gets a different ID
+        nextId++;
     }
 
+    // Getter method to get the user's ID
     public int getId() {
         return id;
     }
 
+    // Getter method to get the user's name
     public String getName() {
         return name;
     }
 
+    // Abstract method - subclasses will need to say what the role is
     public abstract String getRole();
 
+    // toString method to print info about the user in a nice format
     @Override
     public String toString() {
         return getRole() + "[ID = " + id + ", Name = " + name + "]";
